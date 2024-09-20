f55e32ba7ad946cfa3c5f09a46ee4ab0f5f6f370
Alex-sk-dev <68778618+Alex-sk-dev@users.noreply.github.com>
2024-09-20 17:24:07 +0200

    compression and QoL changes

--- euklidovalgoritmus.py
+++ euklidovalgoritmus.py
@@ -3,38 +3,50 @@ def functionalblock():
     endblock = False
     a = int(input("zadaj prvé číslo... "))
     b = int(input("zadaj druhé číslo... "))
+    startA = a
+    startB = b
     oldtest = int(-1)
     while endblock == False:
         if a > b:
             test = a%b
             if test <= 0:
-                endblock = True
-                break
+                c = a/b
+                if c!=0:
+                    oldtest = int(b)
+                    endblock = True
+                    break
+                else:
+                    endblock=True
+                    break
             oldtest = test
             a = b
             b = test
         elif b>a:
             test = b%a
             if test <= 0:
-                endblock = True
-                break
+                c = b/a
+                if c!=0:
+                    oldtest = int(a)
+                    endblock = True
+                    break
+                else:
+                    endblock=True
+                    break
             oldtest=test
             b = a
             a = test
-    if oldtest == 1:
-        print("tieto čísla sú nesúdelitelné")
-    if oldtest == -1:
-        print("tieto čísla sú nesúdelitelné")
-    if oldtest != 1:
-        print(oldtest)
-    if oldtest != -1:
-        print(oldtest)
+    if oldtest == 1 or oldtest == -1:
+        
+        print(f"\nčísla {startA} a {startB} sú nesúdelitelné")
+    else:
+        print(f"\nNajväčší Spoločný Delitel čísel {startA} a {startB} je číslo {oldtest}")
 functionalblock()
 while trueend == False:
-    ask = str(input("chceš pokračovať? [y/n]... "))
+    ask = str(input("\n\nchceš pokračovať? [y/n]... "))
     ask.lower()
     if ask == "y":
         functionalblock()
-    if ask == "n":
+    elif ask == "n":
         trueend = True
-    
+    elif ask != "y" or ask != "n":
+        functionalblock()
