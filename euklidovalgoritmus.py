4f285426265f9279745a20aa2048f248c1d50be5
Alex-sk-dev <68778618+Alex-sk-dev@users.noreply.github.com>
2024-09-20 16:24:14 +0200

    optimisation
    allowed for multiple reruns in one program launch, other QoL changes

--- euklidovalgoritmus.py
+++ euklidovalgoritmus.py
@@ -1,24 +1,38 @@
-from time import sleep
-end = False
-a = int(input("zadaj prvé číslo... "))
-b = int(input("zadaj druhé číslo... "))
-oldtest = int(-1)
-while end == False:
-    if a > b:
-        test = a%b
-        if test <= 0:
-            end = True
-            break
-        oldtest = test
-        a = b
-        b = test
-    elif b>a:
-        test = b%a
-        if test <= 0:
-            end = True
-            break
-        oldtest=test
-        b = a
-        a = test
-print(f"{oldtest}")
-sleep(1000)
+trueend= False
+def functionalblock():
+    endblock = False
+    a = int(input("zadaj prvé číslo... "))
+    b = int(input("zadaj druhé číslo... "))
+    oldtest = int(-1)
+    while endblock == False:
+        if a > b:
+            test = a%b
+            if test <= 0:
+                endblock = True
+                break
+            oldtest = test
+            a = b
+            b = test
+        elif b>a:
+            test = b%a
+            if test <= 0:
+                endblock = True
+                break
+            oldtest=test
+            b = a
+            a = test
+    if oldtest == 1:
+        print("tieto čísla sú nesúdelitelné")
+    if oldtest == -1:
+        print("tieto čísla sú nesúdelitelné")
+    else:
+        print(f"{oldtest}")
+functionalblock()
+while trueend == False:
+    ask = str(input("chceš pokračovať? [y/n]... "))
+    ask.lower()
+    if ask == "y":
+        functionalblock()
+    if ask == "n":
+        trueend = True
+    
